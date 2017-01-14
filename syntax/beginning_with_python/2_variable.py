9eaab572cb22d8dd3021baf48c0c718451742f89
Max Poon <maxpanziyuan@gmail.com>
2017-01-15 00:52:58 +0800

    update codes

--- syntax/beginning_with_python/2_variable.py
+++ syntax/beginning_with_python/2_variable.py
@@ -4,10 +4,15 @@ print(width, height)
 print()
 
 width = 10
-height = 12
+height = 12.5
 print(width)
 print(height)
 print()
 
 width, height = height, width
 print(width, height)
+
+x = True
+print(x)
+y = False
+print(False)
