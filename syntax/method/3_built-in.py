2efd987ab7b12a44ce646be7aa067014f5b2a01e
Max Poon <maxpanziyuan@gmail.com>
2017-01-15 02:00:51 +0800

    add codes

---
+++ syntax/method/3_built-in.py
@@ -0,0 +1,19 @@
+l = [2,3,4,1,5]
+print(max(l))
+print(min(l))
+print(sum(l))
+print(max(1,2))
+print()
+
+l = [True, False, True, False]
+print(all(l))
+print(any(l))
+print()
+
+import random
+print(random.randint(1,10))
+print(random.choice([1,"hi",2]))
+l = [1,2,3,4,5]
+random.shuffle(l)
+print(l)
+print()
