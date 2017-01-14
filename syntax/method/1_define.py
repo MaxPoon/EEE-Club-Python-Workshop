bc49943533f1b6634735d4264f2560d24d0fd687
Max Poon <maxpanziyuan@gmail.com>
2017-01-15 01:47:35 +0800

    finish part5

---
+++ syntax/method/1_define.py
@@ -0,0 +1,15 @@
+def add(a,b):
+	return a+b
+
+x = add(2,5)
+print(x)
+print()
+
+s = add("Hi", " Jack")
+print(s)
+print()
+
+# x = sub(1,2)
+# print(x)
+# def sub(a,b):
+# 	return a-b
---
+++ syntax/method/2_lambda.py
@@ -0,0 +1,5 @@
+f=lambda x: x+1
+print(f(1))
+
+g=lambda name: "hello "+name
+print(g("Jack"))
