6bd976f0fbd857ac8eab7796c778886b26d18894
Max Poon <maxpanziyuan@gmail.com>
2017-01-19 01:04:26 +0800

    add exercise

---
+++ syntax/exercise/sudoku_sample.py
@@ -0,0 +1,66 @@
+import random
+import itertools
+from copy import deepcopy
+
+def make_board():    
+
+	board = None 
+	count = 0
+	while board is None:
+		board = attempt_board()
+	return board
+
+def attempt_board():
+	numbers = list(range(1, 10))
+	n = 9
+	board = [[None for _ in range(n)] for _ in range(n)]
+
+	for i in range(9):
+		for j in range(9):
+			i0, j0 = i - i % 3, j - j % 3 
+			# shuffle the list so that the board won't be the same
+			random.shuffle(numbers)
+			no_solution = True
+			for x in numbers:
+				# check the row, column and the 3*3 square
+				if (x not in board[i] and 
+					all([row[j] != x for row in board]) and 
+					all(x not in row[j0:j0+3] for row in board[i0:i])):     
+					board[i][j] = x
+					no_solution = False
+					break
+			if no_solution:
+				return None
+	return board
+
+def print_board(board):
+	numbers = list(range(1, 10))
+	omit = 5
+	challange = deepcopy(board)
+	for i, j in itertools.product(range(omit), range(9)):
+		x = random.choice(numbers) - 1
+		challange[x][j] = None
+
+	spacer = "++-----+-----+-----++-----+-----+-----++-----+-----+-----++"
+	print (spacer.replace('-','='))
+	for i, line in enumerate(challange):
+		print("||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||"
+			  .format(*(cell or ' ' for cell in line)))
+		if (i + 1) % 3 == 0: print(spacer.replace('-', '='))
+		else: print(spacer)
+	return challange
+
+def print_answers(board):
+	spacer = "++-----+-----+-----++-----+-----+-----++-----+-----+-----++"
+	print (spacer.replace('-','='))
+	for i, line in enumerate(board):
+		print("||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||"
+			  .format(*(cell or ' ' for cell in line)))
+		if (i + 1) % 3 == 0: print(spacer.replace('-', '='))
+		else: print(spacer)
+
+board = make_board()
+print_board(board)
+print("Press any key to see the answer\n")
+input()
+print_answers(board)
