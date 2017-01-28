f239569d18609bacc134567fe6ce6b3c79e066dc
Max Poon <maxpanziyuan@gmail.com>
2017-01-28 21:09:22 +0800

    update code

--- data_science/1_numpy/5_indexing.py
+++ data_science/1_numpy/5_indexing.py
@@ -2,25 +2,29 @@ import numpy as np
 
 r = np.arange(36)
 r.resize((6, 6))
-print("r:")
+print("r:\n")
 print(r)
 print()
 
-print("r[2,2]:")
+print("r[2,2]:\n")
 print(r[2,2])
 print()
 
-print("r[3, 3:6]:")
+print("r[3, 3:6]:\n")
 print(r[3, 3:6])
 print()
 
-print("r[3:4, 3:6]")
+print("r[3:4, 3:6]:\n")
 print(r[3:4, 3:6])
 print()
 
-print("r[:,3:6]:")
+print("r[:,3:6]:\n")
 print(r[:,3:6])
 print()
 
-print("r[r>20]")
+print("r[r>20]:\n")
 print(r[r>20])
+print()
+
+print("r[r[:,2]>10]:\n")
+print(r[r[:,2]>10])
