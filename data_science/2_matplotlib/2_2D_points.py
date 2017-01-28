0419d41f2ebc08032212f12ab4f42f6a1c09c55d
Max Poon <maxpanziyuan@gmail.com>
2017-01-28 17:01:06 +0800

    update code

--- data_science/2_matplotlib/2_2D_points.py
+++ data_science/2_matplotlib/2_2D_points.py
@@ -5,3 +5,25 @@ x = np.linspace(-10,10,21)
 y = x**3
 plt.plot(x,y,'r.')
 plt.show()
+
+z = x**2
+plt.plot(x,z,'r-')
+plt.show()
+
+plt.plot(x,z,'r--')
+plt.show()
+
+plt.plot(x,z,'r+')
+plt.show()
+
+plt.plot(x,z,'bs')
+plt.show()
+
+plt.plot(x,z,'g:')
+plt.show()
+
+plt.plot(x,z,'go')
+plt.show()
+
+plt.plot(x,y,'ro', x,z,'bx')
+plt.show()
