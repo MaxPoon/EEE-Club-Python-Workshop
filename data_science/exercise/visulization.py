cb1955711702f6ea87f7d0e5f5a577c970e0872c
Max Poon <maxpanziyuan@gmail.com>
2017-01-29 10:58:21 +0800

    update code

--- data_science/exercise/visulization.py
+++ data_science/exercise/visulization.py
@@ -2,5 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.load('dataset.npy')
+
+plt.hist(data[:,0], np.linspace(0,30,100))
+plt.show()
+
+plt.hist(data[:,1], np.linspace(0,30,100))
+plt.show()
+
 plt.plot(data[:,0],data[:,1], 'r+')
 plt.show()
